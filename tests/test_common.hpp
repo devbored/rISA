1d1bcd174c0af7cd5dfd2d0a87a403678c968605
Austin Annestrand <austin.annestrand@gmail.com>
2021-05-01 15:12:10 -0500

    Added gtest for unit testing, added conanfile, added test runner script

---
+++ tests/test_common.hpp
@@ -0,0 +1,8 @@
+#ifndef RISA_TEST_COMMON_HPP
+#define RISA_TEST_COMMON_HPP
+
+#include <iostream>
+
+#define GTEST_COUT std::cerr << "\033[0;32m[ INFO     ] \033[0;37m"
+
+#endif // RISA_TEST_COMMON_HPP
